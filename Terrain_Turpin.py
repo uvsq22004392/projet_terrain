40b35d704a0c3fdc8894e9000b540bbd4c0c7899
Martin DIAMANT <martin.diamant@ens.uvsq.fr>
2021-05-10 10:35:36 +0200

    Ajout personnage, déplacement et des commentaires.

--- Terrain_Turpin.py
+++ Terrain_Turpin.py
@@ -23,10 +23,11 @@ TAILLE_CASE = 10
 
 cases = []
 
-T = 5
-p = 50
-n = 4
-k = 1
+T = 5  # Nombre de voisins conversion eau
+p = 50  # Probabilité qu'une case soit de l'eau
+n = 4  # Nombre d'applications de l'automate
+k = 1  # Taille voisinage pour Moore
+personnage = []
 
 racine = tk.Tk()
 
@@ -40,8 +41,8 @@ canvas2.grid(column=1, row=0)
 
 def proba_eau():
     """Gère la probabilité du type de case initial."""
-    feu = rd.randint(0, 100)
-    if feu < p:
+    feu = rd.randint(0, 100)  # Génère un nombre aléatoire
+    if feu < p:  # Compare ce nombre p -> gère la probabilité
         return ["eau"]
     else:
         return ["terre"]
@@ -67,20 +68,35 @@ def refresh():
                                      j * TAILLE_CASE + TAILLE_CASE),
                                     fill=couleur(cases[i][j][0]),
                                     width=0)
+    if personnage != []:  # Si il y a un personnage
+        canvas2.create_rectangle(personnage[0] * TAILLE_CASE,
+                                 personnage[1] * TAILLE_CASE,
+                                 personnage[0] * TAILLE_CASE + TAILLE_CASE,
+                                 personnage[1] * TAILLE_CASE + TAILLE_CASE,
+                                 fill="red",
+                                 width=0)
 
 
 def moore(i, j):
     """Vérifie le nombre de voisins."""
     global cases
-    MOORE = 0
+    if cases[i][j] == ["eau"]:
+        MOORE = - 1  # Permet d'éviter de compter la case en elle-même
+    else:
+        MOORE = 0
+    # range(-k, k + 1) permet d'aller chercher tous les voisins
+    # à k cases de distance
+    # Exemple : k = 1 -> va chercher les cases de i - 1 à i + 1
     for a in range(-k, k + 1):
         for b in range(-k, k + 1):
-            # Evite d'aller out of range
             if (i + a) <= 0 or (i + a) >= len(cases):
+                # Permet d'éviter de sortir de la liste
                 pass
             elif (j + b) <= 0 or (j + b) >= len(cases[i]):
+                # Permet aussi d'éviter de sortir de la liste
                 pass
             elif cases[i + a][j + b] == ["eau"]:
+                # Compte le nombre de cases d'eau
                 MOORE += 1
     return MOORE
 
@@ -90,22 +106,30 @@ def automate():
     global cases
     cases_tempo = [[[] for i in range(LARGEUR // TAILLE_CASE)]
                    for i in range(HAUTEUR // TAILLE_CASE)]
-    for i in range(len(cases)):
-        for j in range(len(cases[i])):
-            if moore(i, j) >= T:
-                cases_tempo[i][j] = ["eau"]
-            else:
-                cases_tempo[i][j] = ["terre"]
-    cases = list(cases_tempo)
+    # cases_tempo est une liste temporaire que l'on modifie
+    # pour ne pas prendre en compte nos modifications
+    # pendant qu'on les effectue
+    for repetition in range(n):  # On applique n fois l'automate
+        for i in range(len(cases)):
+            for j in range(len(cases[i])):
+                if moore(i, j) >= T:  # On applique Moore
+                    cases_tempo[i][j] = ["eau"]
+                else:
+                    cases_tempo[i][j] = ["terre"]
+    cases = list(cases_tempo)  # On remplace cases par notre nouvelle liste
     refresh()  # Ici et pas dans generer_terrain pour pouvoir tester
 
 
 def generer_terrain():
     """Fonctions proba_eau() puis automate() puis refresh()."""
     global cases
+    global personnage
+    # On réinitialise personnage et cases
+    personnage = []
     cases = []
     cases = [[proba_eau() for i in range(LARGEUR // TAILLE_CASE)]
              for i in range(HAUTEUR // TAILLE_CASE)]
+    # On fait proba_eau pour toutes les cases puis automate.
     automate()
 
 
@@ -113,34 +137,75 @@ def taille_grille():
     pass
 
 
-def creation_personnage():
-    pass
+def creation_personnage(event):
+    """Crée un personnage aux coordonnées du curseur."""
+    global personnage
+    x, y = event.x // TAILLE_CASE, event.y // TAILLE_CASE
+    # Permet d'avoir des coordonnées entières
+    # Donc un numéro de case
+    if cases != [] and cases[x][y] == ['terre'] and personnage == []:
+        # Ne marche que si il y a un terrain
+        # Et que l'on clique sur de la terre
+        # Et qu'il n'y a pas déjà un personnage
+        personnage = [x, y]
+        refresh()  # Ça permet d'afficher le personnage
 
 
-def deplacement():
-    pass
+def deplacement(event):
+    """Déplace le personnage en fonction de la flèche appuiyée."""
+    # Etant optimisée avec le cul, la fonction lag ses morts.
+    global personnage
+    direction = event.keysym  # Récupère la touche appuyée
+    i, j = personnage[0], personnage[1]
+    if direction == "Up":
+        j += -1
+        if j < 0:  # Evite d'aller out of range
+            j += 1
+    elif direction == "Down":
+        j += 1
+        if j > HAUTEUR // TAILLE_CASE:
+            j += -1
+    elif direction == "Left":
+        i += -1
+        if i < 0:
+            i += 1
+    elif direction == "Right":
+        i += 1
+        if i < LARGEUR // TAILLE_CASE:
+            i += -1
+    if cases[i][j] == ["terre"]:
+        personnage[0], personnage[1] = i, j
+    # NB : Vérifier les if i<0 etc parce que je suis nul avec ça
+    refresh()
 
 
 def sauvegarde_terrain():
-    """Permet de sauvegarder le terrain dans un txt."""
+    """Permet de sauvegarder le terrain dans un txt. cases est écrit sur
+       la première ligne, personnage sur la seconde."""
     fic = open("sauvegarde.txt", "w")
+    # On ouvre le fichier de sauvegarde en écriture
     for i in range(len(cases)):
         for j in range(len(cases[i])):
             if cases[i][j] == ["eau"]:
-                fic.write("0")
+                fic.write("0")  # On écrit 0 pour les cases d'eau
             elif cases[i][j] == ["terre"]:
-                fic.write("1")
-        fic.write("/")
-    fic.close()
+                fic.write("1")  # On écrit 1 pour les cases de terre
+        fic.write("/")  # On écrit / pour signifier la fin d'une ligne
+    fic.write("\n")  # Va à la ligne
+    fic.write(str(personnage[0]))  # Sauvegarde le personnage
+    fic.write(str(personnage[1]))  # Sauvegarde le personnage
+    fic.close()  # On ferme le fichier (IMPORTANT)
 
 
 def charger_terrain():
     """Charge un terrain depuis le txt."""
     global cases
-    cases = []
-    sous_liste = []
+    global personnage
+    cases = []  # On vide le terrain actuel
+    sous_liste = []  # Sous-liste que l'on placera dans cases
     fic = open("sauvegarde.txt", "r")
-    lignes = fic.readline()
+    # On ouvre le fichier de sauvegarde en lecture
+    lignes = fic.readline()  # Lis une ligne
     for i in range(len(lignes)):
         if lignes[i] == "0":
             sous_liste.append(["eau"])
@@ -149,8 +214,13 @@ def charger_terrain():
         else:
             cases.append(sous_liste)
             sous_liste = []
-    fic.close()
-    refresh()
+    # On ajoute les cases à une sous-liste que l'on place dans
+    # la liste cases, puis on efface la sous-liste quand on voit /
+    # et on recommance.
+    perso = fic.readline()  # Lis la deuxième ligne
+    personnage = [int(perso[0]), int(perso[1])]  # Charge le personnage
+    fic.close()  # Bien penser à fermer le fichier !
+    refresh()  # On actualise pour pouvoir voir le résultat.
 
 
 # Programme principal #
@@ -170,5 +240,10 @@ bouton3.place(x=0, y=160)
 bouton4 = tk.Button(racine, text="Automate (Moore)",
                     font=("helvetica", "10"), command=automate)
 bouton4.place(x=0, y=190)
+
+canvas2.bind("<Button-1>", creation_personnage)
+canvas2.focus_set()  # Pas trop compris pourquoi mais ça marche quand c'est là
+# Si c'est pas là le bind des touches marche pas donc bon ^^
+canvas2.bind("<Key>", deplacement)
 
 racine.mainloop()
