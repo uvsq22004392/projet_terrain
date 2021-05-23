910faf0e168ff77ca36035349e1ce7149fd33ad2
Martin DIAMANT <martin.diamant@ens.uvsq.fr>
2021-05-23 16:27:20 +0200

    Finalisation du projet

--- Terrain_Turpin.py
+++ Terrain_Turpin.py
@@ -13,28 +13,42 @@
 
 import tkinter as tk
 import random as rd
-# Définition des constantes #
+
+
+# Définition des constantes
 
 LARGEUR, HAUTEUR = 500, 500
 
-TAILLE_CASE = 10
 
 # Définition des variables globales #
 
+TAILLE_CASE = 10
 cases = []
+terrain = []
+
+# Valeurs par défaut
 
 T = 5  # Nombre de voisins conversion eau
 p = 50  # Probabilité qu'une case soit de l'eau
 n = 4  # Nombre d'applications de l'automate
 k = 1  # Taille voisinage pour Moore
-personnage = []
+nbr_h = 50
+nbr_l = 50
+choix = False  # Apparaît dans generer_terrain
+
+# Personnage
+
+personnage = []  # Coordonnées du personnage
 ancien_emplacement_personnage = []
+
+# tkinter
+
 racine = tk.Tk()
-global pk
-canvas = tk.Canvas(racine, height=LARGEUR, width=LARGEUR//2, bg="white")
+canvas = tk.Canvas(racine, height=550, width=250, bg="white")
 canvas.grid(column=0)
 canvas2 = tk.Canvas(racine, height=LARGEUR, width=HAUTEUR, bg="white")
 canvas2.grid(column=1, row=0)
+
 
 # Définition des fonctions #
 
@@ -51,11 +65,9 @@ def proba_eau():
 def couleur(lieu):
     """Retourne la couleur en fonction de la case."""
     if lieu == "terre":
-        return "#157120"
-    if lieu == "prairie":
-        return "#1DE034"
+        return "#157120"  # Vert
     if lieu == "eau":
-        return "#1D87E0"
+        return "#1D87E0"  # Bleu
 
 
 def refresh_personnage():
@@ -65,20 +77,27 @@ def refresh_personnage():
     if personnage != []:  # Si il y a un personnage
         a = personnage[0]  # coordonnées x du curseur
         b = personnage[1]  # coordonnées y du curseur
-        pk = canvas2.create_text((a*10+6, b*10), text="\U0001F47D",
-                                 font="MSGothic 9")
+        pk = canvas2.create_rectangle((a * TAILLE_CASE, b * TAILLE_CASE),
+                                      (a * TAILLE_CASE + TAILLE_CASE,
+                                       b * TAILLE_CASE + TAILLE_CASE),
+                                      outline="",
+                                      fill="red")
 
 
 def refresh():
     """Redessine le terrain."""
-    for i in range(HAUTEUR // TAILLE_CASE):
-        for j in range(LARGEUR // TAILLE_CASE):
-            canvas2.create_rectangle(
-                                    (i * TAILLE_CASE, j * TAILLE_CASE),
-                                    (i * TAILLE_CASE + TAILLE_CASE,
-                                     j * TAILLE_CASE + TAILLE_CASE),
-                                    fill=couleur(cases[i][j][0]),
-                                    width=0)
+    global terrain
+    for i in range(len(terrain)):
+        canvas2.delete(terrain[-1])
+        del terrain[-1]
+    for i in range(len(cases)):
+        for j in range(len(cases[i])):
+            terrain.append(canvas2.create_rectangle(
+                                   (i * TAILLE_CASE, j * TAILLE_CASE),
+                                   (i * TAILLE_CASE + TAILLE_CASE,
+                                    j * TAILLE_CASE + TAILLE_CASE),
+                                   fill=couleur(cases[i][j][0]),
+                                   width=0))
     refresh_personnage()
 
 
@@ -109,7 +128,7 @@ def moore(i, j):
 def automate():
     """Vérifie le nombre de voisins avec moore(i, j) de toutes les cases."""
     global cases
-    cases_tempo = [[[] for i in range(LARGEUR // TAILLE_CASE)]
+    cases_tempo = [[[] for i in range(LARGEUR // (LARGEUR // nbr_l))]
                    for i in range(HAUTEUR // TAILLE_CASE)]
     # cases_tempo est une liste temporaire que l'on modifie
     # pour ne pas prendre en compte nos modifications
@@ -125,24 +144,44 @@ def automate():
     refresh()  # Ici et pas dans generer_terrain pour pouvoir tester
 
 
+def valeurs_par_defaut():
+    """Rétablie les valeurs de terrain par défaut."""
+    global T
+    global p
+    global n
+    global k
+    global nbr_h
+    global nbr_l
+    T = 5
+    p = 50
+    n = 4
+    k = 1
+    nbr_h = 50
+    nbr_l = 50
+
+
 def generer_terrain():
     """Fonctions proba_eau() puis automate() puis refresh()."""
     global cases
     global personnage
+    global LARGEUR
+    global HAUTEUR
+    global TAILLE_CASE
+    if choix:  # On récupère les dimensions choisies.
+        appliquer_choix()
+    else:  # On rétablie les valeurs par défaut au cas où
+        # elles aient été modifiées au préalable.
+        valeurs_par_defaut()
+    TAILLE_CASE = HAUTEUR // nbr_h  # Calcule la taille des cases
+    # NB : les cases seront toujours carrés, en fonction de la hauteur.
+    canvas2.config(height=LARGEUR, width=HAUTEUR)
     # On réinitialise personnage et cases
     personnage = []
     cases = []
-    cases = [[proba_eau() for i in range(LARGEUR // TAILLE_CASE)]
+    cases = [[proba_eau() for i in range(LARGEUR // (LARGEUR // nbr_l))]
              for i in range(HAUTEUR // TAILLE_CASE)]
     # On fait proba_eau pour toutes les cases puis automate.
     automate()
-
-
-def taille_grille():
-    pass
-
-#################################################################
-# Fonctions création du personnage et déplacement
 
 
 def creation_personnage(event):
@@ -165,6 +204,7 @@ def effacer(event):
     global personnage
     personnage = []
     canvas2.delete(pk)
+    refresh_personnage()
 
 
 def deplacement(event):
@@ -181,7 +221,7 @@ def deplacement(event):
             j += 1
     elif direction == "Down":
         j += 1
-        if j >= HAUTEUR // TAILLE_CASE:
+        if j >= LARGEUR // (LARGEUR // nbr_l) or j >= HAUTEUR // TAILLE_CASE:
             j += -1
     elif direction == "Left":
         i += -1
@@ -189,7 +229,7 @@ def deplacement(event):
             i += 1
     elif direction == "Right":
         i += 1
-        if i >= LARGEUR // TAILLE_CASE:
+        if i >= HAUTEUR // TAILLE_CASE:
             i += -1
     if cases[i][j] == ["terre"]:
         # Ne se déplace pas sur l'eau
@@ -212,9 +252,6 @@ def annuler_deplacement():
     refresh_personnage()
 
 
-#########################################################################
-
-
 def sauvegarde_terrain():
     """Permet de sauvegarder le terrain dans un txt. cases est écrit sur
        la première ligne, personnage sur la seconde."""
@@ -229,9 +266,8 @@ def sauvegarde_terrain():
         fic.write("/")  # On écrit / pour signifier la fin d'une ligne
     fic.write("\n")  # Va à la ligne
     fic.write(str(personnage[0]))  # Sauvegarde le personnage
-    fic.write(",")
+    fic.write("\n")
     fic.write(str(personnage[1]))  # Sauvegarde le personnage
-    fic.write(".")
     fic.close()  # On ferme le fichier (IMPORTANT)
 
 
@@ -239,10 +275,6 @@ def charger_terrain():
     """Charge un terrain depuis le txt."""
     global cases
     global personnage
-    global posx
-    global posy
-    posx = ""
-    posy = ""
     cases = []  # On vide le terrain actuel
     sous_liste = []  # Sous-liste que l'on placera dans cases
     fic = open("sauvegarde.txt", "r")
@@ -259,32 +291,109 @@ def charger_terrain():
     # On ajoute les cases à une sous-liste que l'on place dans
     # la liste cases, puis on efface la sous-liste quand on voit /
     # et on recommance.
-
-    perso = fic.readline()  # Lis la deuxième ligne
-    i = 0
-    while perso[i] != ",":
-        if perso[i-1] == ",":
-            break
-        posx += perso[i]
-        i += 1
-    while perso[i] != ".":
-        if perso[i] == ",":
-            i += 1
-        posy += perso[i]
-        i += 1
-    personnage[0] = int(posx)
-    personnage[1] = int(posy)
-    # print(posx, posy)
-    # print(personnage[0], personnage[1])
+    # Lis la deuxième ligne
+    personnage = []
+    personnage.append(int(fic.readline()))
+    personnage.append(int(fic.readline()))
     fic.close()  # Bien penser à fermer le fichier !
     refresh()  # On actualise pour pouvoir voir le résultat.
 
 
+def appliquer_choix():
+    """Applique les paramètres choisis par l'utilisateur."""
+    global p
+    global n
+    global T
+    global k
+    global nbr_h
+    global nbr_l
+    global choix
+    global Frame
+    p = int(choixp.get())
+    n = int(choixn.get())
+    T = int(choixT.get())
+    k = int(choixk.get())
+    nbr_h = int(entree2.get())
+    nbr_l = int(entree1.get())
+    choix = False
+    Frame.destroy()
+    canvas2.focus_set()  # Permet d'utiliser le clavier sur le canvas
+
+
+def fenetre_choix():
+    """Affiche une fenêtre permettant à l'utilisateur
+       de paramétrer la génération de terrain."""
+    global entree1
+    global entree2
+    global choixp
+    global choixk
+    global choixT
+    global choixn
+    global Frame
+    global choix
+
+    Frame = tk.Frame(racine, height=0, width=0)
+    Frame.grid(column=3, row=0)
+    Frame.config(height=500, width=250)
+
+    entree1 = tk.Entry(Frame)
+    entree1.place(x=0, y=25)
+    entree1.insert(0, 50)  # Valeur par défaut
+
+    texte1 = tk.Label(Frame, text="Entrez le nombre de cases en largeur")
+    texte1.place(x=0, y=5)
+
+    entree2 = tk.Entry(Frame)
+    entree2.place(x=0, y=70)
+    entree2.insert(0, 50)  # Valeur par défaut
+
+    texte2 = tk.Label(Frame, text="Entrez le nombre de cases en hauteur")
+    texte2.place(x=0, y=50)
+
+    choixp = tk.Entry(Frame)
+    choixp.place(x=0, y=120)
+    choixp.insert(0, 50)  # Valeur par défaut
+
+    texte3 = tk.Label(Frame, text="Entree la valeur de p (en %)")
+    texte3.place(x=0, y=100)
+
+    choixn = tk.Entry(Frame)
+    choixn.place(x=0, y=165)
+    choixn.insert(0, 4)  # Valeur par défaut
+
+    texte4 = tk.Label(Frame, text="Entree la valeur de n")
+    texte4.place(x=0, y=140)
+
+    choixT = tk.Entry(Frame)
+    choixT.place(x=0, y=230)
+    choixT.insert(0, 5)  # Valeur par défaut
+
+    texte5 = tk.Label(Frame, text="Entree la valeur de T")
+    texte5.place(x=0, y=200)
+
+    choixk = tk.Entry(Frame)
+    choixk.place(x=0, y=285)
+    choixk.insert(0, 1)  # Valeur par défaut
+
+    texte4 = tk.Label(Frame, text="Entree la valeur de k")
+    texte4.place(x=0, y=260)
+
+    bouton_generation = tk.Button(Frame, text="Generer le terrain",
+                                  command=generer_terrain)
+    bouton_generation.place(x=100, y=350)
+
+    choix = True
+
+
 # Programme principal #
 
-
-bouton1 = tk.Button(racine, text="Génération de terrain aléatoire",
+bouton0 = tk.Button(racine, text="Génération de terrain aléatoire par défaut",
                     font=("helvetica", "10"), command=generer_terrain)
+bouton0.place(x=0, y=70)
+
+
+bouton1 = tk.Button(racine, text="Modifier les paramètres de la génération",
+                    font=("helvetica", "10"), command=fenetre_choix)
 bouton1.place(x=0, y=100)
 
 bouton2 = tk.Button(racine, text="Sauvegarde du terrain",
@@ -303,12 +412,13 @@ bouton5 = tk.Button(racine, text="Annuler déplacement",
                     font=("helvetica", "10"), command=annuler_deplacement)
 bouton5.place(x=0, y=220)
 
+
 # Liaison des evenements
 
 canvas2.bind("<Button-1>", creation_personnage)
-canvas2.focus_set()  # Pas trop compris pourquoi mais ça marche quand c'est là
-# Si c'est pas là le bind des touches marche pas donc bon ^^
+canvas2.focus_set()  # Permet d'utiliser le clavier sur le canvas
 canvas2.bind("<Key>", deplacement)
 canvas2.bind("<Double-Button-1>", effacer)
+
 
 racine.mainloop()
